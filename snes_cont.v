4d3f652cbb578a265325d3005c1564450764f660
Jacob Sanders <jacob.sanders@utah.edu>
2010-11-17 01:42:37 +0000

    Empty unfinished controller input module.

---
+++ snes_cont.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:35:06 11/16/2010 
+// Design Name: 
+// Module Name:    snes_cont 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module snes_cont(
+    input clk,
+    input rst,
+    output data_pulse,
+    output data_latch,
+    input data_in 
+    );
+
+
+endmodule
